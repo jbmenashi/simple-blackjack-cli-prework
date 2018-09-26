ea29e17e5a92bb61ba12162f7d37eae9db0f0886
Jacob Menashi <jbmenashi@gmail.com>
2018-09-26 22:28:17 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,7 +35,7 @@ def hit?(card_total)
   answer = get_user_input
   if answer == 'h'
     card_total = card_total + deal_card
-    puts card_total
+    card_total
   elsif answer == 's'
     card_total
   else
